a4187b829af1d11957aece0024a284edf0973960
hilal-cerit <77547891+hilal-cerit@users.noreply.github.com>
2021-02-08 21:54:38 +0300

    Add files via upload

---
+++ MyList/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyList
+{
+   
+    class MyDictionary<T>
+    {
+        T[] _array;
+
+        public MyDictionary()
+        {
+            _array = new T[0];
+           
+        }
+
+        public void Add(T item)
+        {
+            T[] _tempoArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempoArray.Length; i++)
+            {
+                _array[i]= _tempoArray[i] ;
+
+            }
+            _array[_array.Length - 1] = item;
+        }
+        public void Read()
+        { foreach (var item in _array)
+            { Console.WriteLine(item); }
+        }
+        
+
+
+    }
+
+   
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+
+            myDictionary.Add("hello");
+            myDictionary.Read();
+
+
+
+
+        }
+    }
+}
